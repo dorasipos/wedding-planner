827680da3f639b324dc8d95ed42632315ce68c43
Dora <sipos_dora@yahoo.com>
2016-03-24 19:24:26 +0200

    Add solution to source control

---
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.Model/Wedding.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dora.WeddingPlanner.Model
+{
+    public class Wedding
+    {
+        public IEnumerable<WeddingTask> Tasks { get; }
+
+        public IEnumerable<WeddingGuest> Guests { get; }
+    }
+}
