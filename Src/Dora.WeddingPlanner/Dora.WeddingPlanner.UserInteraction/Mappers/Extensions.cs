75a35813426c78b4e9ae2942d0a8a37897c41f94
Hintea Dan Alexandru <hintea_dan@yahoo.co.uk>
2016-03-31 01:59:25 +0300

    Scaffold user interaction idea

---
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.UserInteraction/Mappers/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using Dora.WeddingPlanner.Model;
+using Dora.WeddingPlanner.UserInteraction.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dora.WeddingPlanner.UserInteraction.Mappers
+{
+    internal static class Extensions
+    {
+        public static Person Map(this PersonDto dto)
+        {
+            return DtoToModelMapper.Map(dto);
+        }
+    }
+}
---
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.UserInteraction/UseCase/WeddingUseCase.cs
@@ -0,0 +1,19 @@
+﻿using Dora.WeddingPlanner.Model;
+using Dora.WeddingPlanner.UserInteraction.Model;
+using Dora.WeddingPlanner.UserInteraction.Mappers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dora.WeddingPlanner.UserInteraction.UseCase
+{
+    public class WeddingUseCase
+    {
+        public Wedding DefineNew(PersonDto bride, PersonDto groom)
+        {
+            return new Wedding(bride.Map(), groom.Map());
+        }
+    }
+}
