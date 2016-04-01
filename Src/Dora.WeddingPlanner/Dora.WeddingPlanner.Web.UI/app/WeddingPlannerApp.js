0fc14fae1476a0313e1b46362d291c15d0d6fc17
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-04-01 12:03:28 +0300

    OD: Create wedding template

--- Src/Dora.WeddingPlanner/Dora.WeddingPlanner.Web.UI/app/WeddingPlannerApp.js
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.Web.UI/app/WeddingPlannerApp.js
@@ -1,9 +1,15 @@
 ﻿(function (ng) {
     'use strict';
 
-    ng.module('WeddingPlannerApp', ['ng', 'ngMaterial'])
-    .config(['$mdThemingProvider', function ($mdThemingProvider) {
+    ng.module('WeddingPlannerApp', ['ng', 'ngRoute', 'ngMaterial'])
+    .config(['$mdThemingProvider', '$routeProvider', '$locationProvider', function ($mdThemingProvider, $route, $location) {
         $mdThemingProvider.theme('default');
+
+        $location.html5Mode(false);
+
+        $route
+            .when('/new', { templateUrl: 'views/define-new-wedding.tmpl.html' });
+
     }]);
 
 })(this.angular);
