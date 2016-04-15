620e2ca7270ae40ba9412259a793af93d9a39871
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-04-15 10:28:54 +0300

    Logging extensions

---
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.Logging/Extensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NLog;
+
+namespace Dora.WeddingPlanner.Logging
+{
+    public static class Extensions
+    {
+        public static LogTiming Timing(this Logger logger, string actionName, LogLevel logLevel)
+        {
+            return new LogTiming(logger, actionName, logLevel);
+        }
+
+        public static LogTiming Timing(this Logger logger, string actionName)
+        {
+            return new LogTiming(logger, actionName);
+        }
+    }
+}
