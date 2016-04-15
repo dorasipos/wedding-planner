3618691658274eaed9ee700893a001df7d91c343
Hintea Dan Alexandru <dan.hintea@recognos.ro>
2016-04-15 14:19:41 +0300

    Log CQ exceptions

--- Src/Dora.WeddingPlanner/Dora.WeddingPlanner.UserInteraction/CommandAndQueryRunner.cs
+++ Src/Dora.WeddingPlanner/Dora.WeddingPlanner.UserInteraction/CommandAndQueryRunner.cs
@@ -6,11 +6,14 @@ using System.Threading.Tasks;
 using Dora.WeddingPlanner.UserInteraction.Commands;
 using Dora.WeddingPlanner.UserInteraction.Queries;
 using Dora.WeddingPlanner.Tracing;
+using NLog;
 
 namespace Dora.WeddingPlanner.UserInteraction
 {
     internal static class CommandAndQueryRunner
     {
+        private static Logger log = LogManager.GetCurrentClassLogger();
+
         public static CommandResult<TResult> Run<TResult>(ImAnInteractionCommand<TResult> command)
         {
             Exception exception;
@@ -18,6 +21,7 @@ namespace Dora.WeddingPlanner.UserInteraction
 
             if (!new Func<TResult>(() => command.Execute()).Try(out result, out exception))
             {
+                log.Error("Error running command {1}{0}Details{0}========={0}{2}{0}{3}", Environment.NewLine, command, exception.Message, exception.StackTrace);
                 return new ExceptionCommandResult<TResult>(exception);
             }
 
@@ -31,6 +35,7 @@ namespace Dora.WeddingPlanner.UserInteraction
 
             if (!new Func<TResult>(() => query.Query(parameter)).Try(out result, out exception))
             {
+                log.Error("Error running query {1}{0}Details{0}========={0}{2}{0}{3}", Environment.NewLine, query, exception.Message, exception.StackTrace);
                 return new ExceptionQueryResult<TResult>(exception);
             }
 
